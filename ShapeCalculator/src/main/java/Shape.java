4eea955e37c9c86c276b6cdac66ef138b2641ecb
Krishna Uprit <125099508+Krishnauprit18@users.noreply.github.com>
2024-06-18 18:15:11 +0530

    Update Shape.java

--- ShapeCalculator/src/main/java/Shape.java
+++ ShapeCalculator/src/main/java/Shape.java
@@ -1,7 +1,26 @@
 package main.java;
 
+/**
+ * Abstract class representing a geometric shape.
+ */
 abstract class Shape{
+
+    /**
+     * Calculates the area of the shape.
+     * @return The area of the shape
+     */
     abstract double calculateArea();
+
+    /**
+     * Calculates the Perimeter of the shape.
+     * @return The perimeter of the shape
+     */
     abstract double calculatePerimeter();
+
+    /**
+     * Calculates the Volume of the shape.
+     * Only applicable for 3-D shapes
+     * @return The volume of the shape
+     */
     abstract double calculateVolume();
 }

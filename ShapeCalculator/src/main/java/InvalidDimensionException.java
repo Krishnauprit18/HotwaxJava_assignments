6b2a9ef4bf9796a52aa495b74863073b2429652a
Krishna Uprit <125099508+Krishnauprit18@users.noreply.github.com>
2024-06-18 18:14:11 +0530

    Update InvalidDimensionException.java

--- ShapeCalculator/src/main/java/InvalidDimensionException.java
+++ ShapeCalculator/src/main/java/InvalidDimensionException.java
@@ -1,5 +1,8 @@
 package main.java;
 
+/**
+ * Exception thrown when a dimension of a shape is invalid.
+ */
 class InvalidDimensionException extends Exception{
     InvalidDimensionException(String message){
         super(message);
